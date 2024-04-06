53b4bd6adeb6181808532f803abfe253a7f85e75
Nick Lu <nicklu717@gmail.com>
2024-04-06 17:10:40 +0800

    [feat] implement SingleLinkedList

---
+++ DataStructure/LinkedList/SingleLinkedList.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+template <class Element>
+class SingleLinkedList {
+private:
+    class Node {
+    public:
+        Element element;
+        Node* next;
+
+        Node(const Element& element):
+            element(element),
+            next(nullptr) {}
+    };
+
+    Node* head;
+
+public:
+    SingleLinkedList(): head(nullptr) {}
+    ~SingleLinkedList() {
+        Node* currentNode = head;
+        while (currentNode) {
+            Node* nextNode = currentNode->next;
+            delete currentNode;
+            currentNode = nextNode;
+        }
+    }
+
+    void append(const Element& element) {
+        if (head) {
+            Node* currentNode = head;
+            while (currentNode->next) {
+                currentNode = currentNode->next;
+            }
+            currentNode->next = new Node(element);
+        } else {
+            head = new Node(element);
+        }
+    }
+
+    // bool insert(const Element& element, const int& index) {}
+    
+    // Element* element(const int& index) {}
+    
+    // Element* remove(const int& index) {}
+
+    void print() const {
+        if (head) {
+            Node* currentNode = head;
+            while (currentNode) {
+                std::cout << currentNode->element << std::endl;
+                currentNode = currentNode->next;
+            }
+        } else {
+            cout << "NULL" << endl;
+        }
+    }
+};
