04af88349470f72ce79d8f68bea4f1f1fb78b218
Nick Lu <nicklu717@gmail.com>
2024-04-07 17:17:29 +0800

    [test] add SingleLinkedList testing method

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
+#include "DataStructure/LinkedList/SingleLinkedList.cpp"
+
+void testSingleLinkedList() {
+    SingleLinkedList<int> list = SingleLinkedList<int>();
+    list.append(3);
+    list.append(5);
+    list.append(6);
+    list.append(8);
+    list.append(1111);
+    list.print();
+}
+
 int main() {
+    testSingleLinkedList();
+
     return 0;
 }
