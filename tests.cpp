be74ab966fdbd6fe94960c6e3f336a82b5380667
Nick Lu <nicklu717@gmail.com>
2024-04-28 15:46:01 +0800

    [feat] add a testing test file

---
+++ tests.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+#include "DataStructure/LinkedList/SingleLinkedList.cpp"
+
+TEST(LinkedListTest, Append) {
+    ASSERT_EQ(1, true) << "1 not equal to true";
+    EXPECT_EQ(0, false) << "0 not equal to false";
+}
