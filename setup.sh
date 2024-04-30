a87821d3a9635e54af0c5f2adbffb750d62beb2c
Nick Lu <nicklu717@gmail.com>
2024-04-30 14:25:05 +0800

    [feat] add logs in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
+echo "===== Executing command: `git submodule init` =====" && \
 git submodule init && \
+echo "===== Finished command: `git submodule init` =====" && \
+
+echo "===== Executing command: `git submodule update` =====" && \
 git submodule update && \
+echo "===== Finished command: `git submodule update` =====" && \
+
+echo "===== Executing command: `./tools/vcpkg/bootstrap-vcpkg.sh` =====" && \
 ./tools/vcpkg/bootstrap-vcpkg.sh && \
-./tools/vcpkg/vcpkg install gtest
+echo "===== Finished command: `./tools/vcpkg/bootstrap-vcpkg.sh` =====" && \
+
+echo "===== Executing command: `./tools/vcpkg/vcpkg install gtest` =====" && \
+./tools/vcpkg/vcpkg install gtest && \
+echo "===== Finished command: `./tools/vcpkg/vcpkg install gtest` =====" && \
